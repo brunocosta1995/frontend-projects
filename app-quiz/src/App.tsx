ff95996cfa172a295c37defae0354764dd1ae80b
Bruno Costa <costabruno.dev@gmail.com>
2024-07-11 12:30:42 -0300

    primeiro commit

---
+++ app-quiz/src/App.tsx
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import QuestionCard from "./Components/QuestionCard";
+
+const TOTAL_QUESTIONS = 10;
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+
+  async function startTrivia() {}
+
+  function checkAnswer(e: React.MouseEvent<HTMLButtonElement>) {}
+
+  function nextQuestion() {}
+
+  return (
+    <div className="App">
+      <h1>React Quiz App</h1>
+      <button className="start" onClick={startTrivia}>
+        Start Trivia
+      </button>
+      <p className="score">Score: </p>
+      <p>Loading Next Question...</p>
+      <QuestionCard
+        questionNumber={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        callback={checkAnswer}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+      />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
+}
+
+export default App;
